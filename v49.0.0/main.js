9adcaefad2671e5445d6d9025e039d61c153df5d
alexiswilliams <alexiswilliams2000@yahoo.com>
2020-10-14 15:59:20 -0400

    Updates for job 300 - 349

--- v49.0.0/main.js
+++ v49.0.0/main.js
@@ -24,7 +24,7 @@ function sleep(milliseconds) {
 var i = 1;
 fs.readdirSync(uploadFilesFolder).forEach(file => {
     if (file.endsWith(CSV_FILE)) {
-        if (i > 249 && i < 300) {
+        if (i > 299 && i < 350) {
             console.log(i + '\t\t:' + file);
             var execCommandIteration = execCommand.replace('{dataAccess}', file).replace('{outputSuccess}', file).replace('{outputError}', file);
             child = exec(execCommandIteration,
