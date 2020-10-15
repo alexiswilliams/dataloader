1fe4b3a44cdbf2721531603a69ee5e6b6231c901
alexiswilliams <alexiswilliams2000@yahoo.com>
2020-10-14 22:52:03 -0400

    Run 549 - 600

--- v49.0.0/main.js
+++ v49.0.0/main.js
@@ -24,7 +24,7 @@ function sleep(milliseconds) {
 var i = 1;
 fs.readdirSync(uploadFilesFolder).forEach(file => {
     if (file.endsWith(CSV_FILE)) {
-        if (i > 499 && i < 550) {
+        if (i > 549 && i < 600) {
             console.log(i + '\t\t:' + file);
             var execCommandIteration = execCommand.replace('{dataAccess}', file).replace('{outputSuccess}', file).replace('{outputError}', file);
             child = exec(execCommandIteration,
